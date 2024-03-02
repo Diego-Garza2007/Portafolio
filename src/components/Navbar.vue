4c3e0e4bdc1ded086f8f32b11964d3bdab231c1a
Diego Garza <diegoe.garza2007@gmail.com>
2024-03-02 11:19:13 -0600

    1.15 Navbar Responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,11 +4,19 @@
   const isDark = useDark()
   const toggleDark = useToggle(isDark)
 
+  function AbrirNav() {
+    const mobile = document.querySelector('#mobile_select')
+    mobile.classList.toggle('nav__mobile')
+    mobile.classList.toggle('None')
+  }
+
 </script>
 
 <template>
       <nav class="nav">
     <img src="../assets/dev-removebg-preview-1.svg">
+    <div id="mobile_select" class="None nav__desktop">
+      <div class="nav__mobileflex">
     <ul>
         <li class="nav__enlace"><a href="#home">{{ $t("nav.inicio") }}</a></li>
         <li class="nav__enlace"><a href="#about">{{ $t("nav.sobre") }}</a></li>
@@ -20,20 +28,21 @@
     
     <div class="nav__line"></div>
 
-      <div class="nav__divIcons" >
-
+      <div class="nav__divIcons">
+        
            <LanguageSwicht />
-
+        
           <button class="nav__button Imoon" @click="toggleDark()" >
             <font-awesome-icon v-if="isDark === false" icon="fa-solid fa-moon" class="fa-2xl" />
            
             <font-awesome-icon v-else icon="fa-solid fa-sun" class="fa-2xl"/>
           </button>
-          
-          <div class="nav__iconnav">
-            <font-awesome-icon :icon="['fas', 'bars']" class="fa-2xl"/>
           </div>
-
-      </div>
+    </div>
+  </div>
+            <div class="nav__iconnav" @click="AbrirNav">
+                    <font-awesome-icon :icon="['fas', 'bars']" class="fa-2xl"/>
+            </div>
+            
     </nav>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -88,10 +88,10 @@
    
       
         <input type="email" name="email" class="container-contacto__email" placeholder="E-mail">
-        <textarea name="message" v-bind:placeholder="$t('body.contactame')" class="container-contacto__desc"></textarea>
+        <textarea name="message" v-bind:placeholder=" $t('body.contactame')" class="container-contacto__desc"></textarea>
   
     <!-- your other form fields go here -->
-    <button type="submit" class="container-contacto__submit">{{ $t('body.enviar') }}</button>
+    <button type="submit" class="container-contacto__submit">{{  $t('body.enviar') }}</button>
     </form>
     </div>
     </section>
